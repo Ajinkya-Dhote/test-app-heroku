e07012a6bfe49d9816e03887eaa13e92d339bcc5
Ajinkya <14mcei01@nirmauni.ac.in>
2017-09-15 22:00:13 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
- // set up ========================
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    // var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-    // configuration =================
+var express = require('express');
+var app = express();
 
-    // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+app.set('port', (process.env.PORT || 5000));
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
 
-    // listen (start app with node server.js) ======================================
-    app.listen(80);
-    console.log("App listening on port 80");
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
