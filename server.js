b07cb8ad0f41d8398f3566b7a4df26382d5f63e6
Ajinkya Dhote <ajinkyad@winsoftech.com>
2019-08-18 19:46:46 +0530

    fix

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 
@@ -11,36 +10,36 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
-app,post('/webhooks', function(request, response) => {
+app.post('/webhooks', function(request, response) {
     response.setHeader('Content-Type', 'application/json');
     let result = "";
     let resObj = {
         "fulfillmentText": "This is a text response",
-        "fulfillmentMessages": [
+        "fulfillmentMessages": [{
             "text": {
                 "text": "Hi am i am from webhook"
-            },
-         "payload": {
-  "google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
-        {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response from webhook"
-          }
-        }
-      ]
-    }
-  }
-}
-        ]
-    }
+            }
+        }],
+        "payload": {
+            "google": {
+                "expectUserResponse": true,
+                "richResponse": {
+                    "items": [{
+                        "simpleResponse": {
+                            "textToSpeech": "this is a simple response from webhook"
+                        }
+                    }]
+                }
+            }
+        },
+        "source": ""
+    };
+    return response.json(resObj);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
